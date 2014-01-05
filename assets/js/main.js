5003cdafd35d99a1c59b73ccdd1594e3cd3448a4
caiguanhao <caiguanhao@gmail.com>
2014-01-06 01:55:22 +0800

    allow arrays of properties to show and parse;

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,26 +41,45 @@ $(function(){
   }
   function buildInfoForm() {
     var manifest_variables = manifest.__variables__;
+    function textInput(name, val) {
+      return '<input name="' + name + '" value="' + (val || '') + '">';
+    }
+    function selectOptions(name, options, selected) {
+      var output = '<select name="' + name + '">'
+      $.each(options, function(key, value) {
+        output += '<option value="' + value + '"' +
+          (selected && selected === value ? ' selected' : '') +
+            '>' + value + '</option>';
+      });
+      output += '</select>';
+      return output;
+    }
     function build(variables, defaults) {
       var output = '';
       $.each(variables, function(var_name, var_content) {
         var defval = defaults && defaults.hasOwnProperty(var_name) ? defaults[var_name] : null;
         var isArray = (var_content instanceof Array && typeof(var_content[0]) === 'object');
-        output += '<div class="input" data-is-array="' + isArray + '">';
-        output += '<span class="label">' + var_name + '</span>';
+        output += '<div class="input" data-name="' + var_name + '" data-is-array="' + isArray + '">';
+        if (!isArray) {
+          output += '<span class="label">' + var_name + '</span>';
+        }
         if (typeof(var_content) === 'string') {
-          output += '<input name="' + var_name + '" value="' + (defval || '') + '">';
+          output += textInput(var_name, defval);
         }
         if (typeof(var_content) === 'object') {
           if (var_content instanceof Array) {
             if (typeof(var_content[0]) === 'object') {
-              output += build(var_content[0], defval ? defval[0] : null);
+              if (defval instanceof Array) {
+                $.each(defval, function(index) {
+                  output += '<div class="item"><span class="label">' + var_name + (index+1) + '</span>';
+                  output += build(var_content[0], defval ? defval[index] : null) + '</div>';
+                });
+              } else {
+                output += '<div class="item"><span class="label">' + var_name + '</span>';
+                output += build(var_content[0], null) + '</div>';
+              }
             } else {
-              output += '<select name="' + var_name + '">'
-              $.each(var_content, function(key, value) {
-                output += '<option value="' + value + '">' + value + '</option>';
-              });
-              output += '</select>';
+              output += selectOptions(var_name, var_content, defval);
             }
           } else {
             output += build(var_content, defval);
@@ -73,28 +92,28 @@ $(function(){
     $('#info').html(build(manifest_variables, testing));
   }
   function formToObject() {
-    function get(elements) {
-      var output = {};
-      elements.children().each(function() {
-        if ($(this).children().length === 0) return true;
-        var name = $('> .label', this).text();
-        if (!name) return true;
-        var input = $('.input', this);
-        if (input.length === 0) {
-          output[name] = $('input, select', this).val()
+    var obj = {};
+    function modify(object, parents, index, val) {
+      var current = parents.eq(index);
+      var is_array = current.data('is-array');
+      var name = current.data('name');
+      if (!name) name = current.index();
+      if (index > 0) {
+        if (is_array === true) {
+          object[name] = object[name] || [];
         } else {
-          var newObj = {};
-          if ($(this).data('is-array') === true) {
-            newObj[name] = [ get($(this)) ];
-          } else {
-            newObj[name] = get($(this));
-          }
-          $.extend(output, newObj);
+          object[name] = object[name] || {};
         }
-      });
-      return output;
+        modify(object[name], parents, index - 1, val);
+      } else {
+        object[name] = val;
+      }
     }
-    return get($('#info'));
+    $('#info').find('input, select').each(function() {
+      var parents = $(this).parentsUntil('#info');
+      modify(obj, parents, parents.length - 1, $(this).val());
+    });
+    return obj;
   }
   function parse(what) {
     function formToTplData(parent) {
