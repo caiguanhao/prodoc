d27263d40bc82125ac04f70303f2ece0da46ab0b
caiguanhao <caiguanhao@gmail.com>
2014-01-06 13:13:08 +0800

    improve merged view; copy script from previous view;

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,6 +20,7 @@ $(function(){
   // merged view:
   function startMergedView() {
     buildDocument();
+    buildForm();
     parseDocument();
     bindEvents();
 
@@ -46,14 +47,108 @@ $(function(){
         $('#main').append(block_dom);
       });
     }
+    function buildForm() {
+      var manifest_variables = manifest.__variables__;
+      function textInput(name, val) {
+        return '<input name="' + name + '" value="' + (val || '') + '">';
+      }
+      function selectOptions(name, options, selected) {
+        var output = '<select name="' + name + '">'
+        $.each(options, function(key, value) {
+          output += '<option value="' + value + '"' +
+            (selected && selected === value ? ' selected' : '') +
+              '>' + value + '</option>';
+        });
+        output += '</select>';
+        return output;
+      }
+      function build(variables, defaults) {
+        var output = '';
+        $.each(variables, function(var_name, var_content) {
+          var defval = defaults && defaults.hasOwnProperty(var_name) ? defaults[var_name] : null;
+          var isArray = (var_content instanceof Array && typeof(var_content[0]) === 'object');
+          output += '<div class="input" data-name="' + var_name + '" data-is-array="' + isArray + '">';
+          if (!isArray) {
+            output += '<span class="label">' + var_name + '</span>';
+          }
+          if (typeof(var_content) === 'string') {
+            output += textInput(var_name, defval);
+          }
+          if (typeof(var_content) === 'object') {
+            if (var_content instanceof Array) {
+              if (typeof(var_content[0]) === 'object') {
+                if (defval instanceof Array) {
+                  $.each(defval, function(index) {
+                    output += '<div class="item"><span class="label">' + var_name + (index+1) + '</span>';
+                    output += build(var_content[0], defval[index]) + '</div>';
+                  });
+                } else {
+                  output += '<div class="item untouched"><span class="label">' + var_name + '</span>';
+                  output += build(var_content[0], null) + '</div>';
+                }
+              } else {
+                output += selectOptions(var_name, var_content, defval);
+              }
+            } else {
+              output += build(var_content, defval);
+            }
+          }
+          output += '</div>'
+        });
+        return output;
+      }
+      $('#info').html(build(manifest_variables, testing));
+    }
+    function formToObject() {
+      var obj = {};
+      function modify(object, parents, index, val) {
+        var current = parents.eq(index);
+        if (current.hasClass('untouched')) return;
+        var is_array = current.data('is-array');
+        var name = current.data('name');
+        if (!name) name = current.index();
+        if (index > 0) {
+          if (is_array === true) {
+            object[name] = object[name] || [];
+          } else {
+            object[name] = object[name] || {};
+          }
+          modify(object[name], parents, index - 1, val);
+        } else {
+          object[name] = val;
+        }
+      }
+      $('#info').find('input, select').each(function() {
+        var parents = $(this).parentsUntil('#info');
+        modify(obj, parents, parents.length - 1, $(this).val());
+      });
+      return obj;
+    }
+    function touchFormBlock(element) {
+      var item = $(element).closest('.item');
+      var str = item.find('input').map(function() { return $(this).val(); }).toArray().join('');
+      if (str) {
+        item.removeClass('untouched');
+      } else {
+        item.addClass('untouched');
+      }
+    }
     function parseDocument() {
       $('#main').find('.item').each(function() {
         var tpl_data = {};
         tpl_data[$(this).data('template-name')] = $(this).data('template');
-        $(this).html(toHTML(Handlebars, manifest.__templates__, tpl_data, testing));
+        $(this).html(toHTML(Handlebars, manifest.__templates__, tpl_data, formToObject()));
       });
     }
     function bindEvents() {
+      $(document).on('keyup keydown', '#info input, #info [contenteditable]', function(e) {
+        touchFormBlock(this);
+        parseDocument();
+      });
+      $(document).on('change', '#info select', function(e) {
+        touchFormBlock(this);
+        parseDocument();
+      });
       $(document).on('click', '#main .item', function(e) {
         e.stopPropagation();
         e.preventDefault();
